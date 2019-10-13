f4b0c3ca763b02be2c8888146c02039e65f93860
Viktor <viktorprodeus@gmail.com>
2019-10-14 01:31:14 +0300

    feat: добавил функцию для загрузки данных из формы на сервер

--- js/backend.js
+++ js/backend.js
@@ -2,6 +2,7 @@
 
 (function () {
   var URL_LOAD = 'https://js.dump.academy/kekstagram/data';
+  var URL_UPLOAD = 'https://js.dump.academy/kekstagram';
   var STATUS_OK = 200;
   var SET_TIME = 10000; // 10s
 
@@ -28,6 +29,30 @@
 
       xhr.open('GET', URL_LOAD);
       xhr.send();
+    },
+
+    upload: function (data, onSuccess, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === STATUS_OK) {
+          onSuccess(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = SET_TIME;
+
+      xhr.open('POST', URL_UPLOAD);
+      xhr.send(data);
     }
   };
 })();
